dd40b2f5d0f86c2b1d35e51c1f893b26f5395745
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-12-25 16:26:00 +0200

    adds state.status

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -7,8 +7,10 @@ import Loader from '../Loader/Loader';
 class ImageGallery extends Component {
   state = {
     gallery: null,
-    loading: false,
+    error: null,
+    status: 'idle',
   };
+
   //   componentDidMount() {
   //     fetch(
   //       `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=30833222-94e556fd2dbde651348f500b2&image_type=photo&orientation=horizontal&per_page=12`
@@ -19,21 +21,46 @@ class ImageGallery extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
       fetch(
         `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=30833222-94e556fd2dbde651348f500b2&image_type=photo&orientation=horizontal&per_page=12`
       )
-        .then(res => res.json())
-        .then(gallery => this.setState({ gallery }))
-        .finally(this.setState({ loading: false }));
+        .then(response => response.json())
+        .then(gallery => {
+          if (gallery.hits.length > 0) {
+            return this.setState({ gallery, status: 'resolved' });
+          }
+
+          return Promise.reject(
+            new Error(
+              `По запиту <${this.props.searchQuery}> зображення не знайдено`
+            )
+          );
+        })
+        .catch(error => {
+          console.log(error);
+          return this.setState({ error, status: 'rejected' });
+        });
     }
   }
 
   render() {
     const galleryList = this.state.gallery;
-    return (
-      <div>
-        {this.state.loading && <Loader />}
+    if (this.state.status === 'idle') {
+      return <div>Введіть запит для пошуку</div>;
+    }
+
+    if (this.state.status === 'pending') {
+      return <Loader />;
+    }
+
+    if (this.state.status === 'rejected') {
+      console.log(this.state.error);
+      return <h1>{this.state.error.message}</h1>;
+    }
+
+    if (this.state.status === 'resolved') {
+      return (
         <ul className={css.gallery}>
           {galleryList &&
             galleryList.hits.map(item => (
@@ -42,8 +69,8 @@ class ImageGallery extends Component {
               </li>
             ))}
         </ul>
-      </div>
-    );
+      );
+    }
   }
 }
 
