30d9c5813b45867efc909c5f649884405b2230ea
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-12-25 03:10:27 +0200

    adds loader

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,33 +2,47 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from '../styles.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Loader from '../Loader/Loader';
 
 class ImageGallery extends Component {
   state = {
     gallery: null,
+    loading: false,
   };
+  //   componentDidMount() {
+  //     fetch(
+  //       `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=30833222-94e556fd2dbde651348f500b2&image_type=photo&orientation=horizontal&per_page=12`
+  //     )
+  //       .then(res => res.json())
+  //       .then(gallery => this.setState({ gallery }));
+  //   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
+      this.setState({ loading: true });
       fetch(
         `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=30833222-94e556fd2dbde651348f500b2&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(res => res.json())
-        .then(gallery => this.setState({ gallery }));
+        .then(gallery => this.setState({ gallery }))
+        .finally(this.setState({ loading: false }));
     }
   }
 
   render() {
     const galleryList = this.state.gallery;
     return (
-      <ul className={css.gallery}>
-        {galleryList &&
-          galleryList.hits.map(item => (
-            <li className={css.gallery_item} key={item.id}>
-              <ImageGalleryItem item={item} />
-            </li>
-          ))}
-      </ul>
+      <div>
+        {this.state.loading && <Loader />}
+        <ul className={css.gallery}>
+          {galleryList &&
+            galleryList.hits.map(item => (
+              <li className={css.gallery_item} key={item.id}>
+                <ImageGalleryItem item={item} />
+              </li>
+            ))}
+        </ul>
+      </div>
     );
   }
 }
